13a2fb86806111755cd04ce7a6753bf5d1a85c89
Brandon Levy <brandon.levy@mail.mcgill.ca>
2018-01-03 16:03:05 -0500

    added click event to summon cta button

---
+++ cats.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+  var summon = document.querySelector('.summon-cats')
+  summon.addEventListener("click", function(e){
+    console.log("meow");
+  })
+
+
+
+
+
+
+
+});
