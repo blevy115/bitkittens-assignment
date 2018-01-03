8e1a436224d8e0e287546ffe74860072cfcad5f8
Brandon Levy <brandon.levy@mail.mcgill.ca>
2018-01-03 16:17:17 -0500

    added cats to the DOM

--- cats.js
+++ cats.js
@@ -1,14 +1,23 @@
 document.addEventListener('DOMContentLoaded', function() {
 
   var summon = document.querySelector('.summon-cats')
+  var catBoxes = document.querySelectorAll('.cat-box')
   summon.addEventListener("click", function(e){
-    console.log("meow");
-  })
-
-
-
-
-
+    $.ajax({
+      url: 'http://bitkittens.herokuapp.com/cats.json',
+      method: 'GET',
+      datatype: 'json'
+    }).done(function(data){
+      var i = 0
+      data.cats.forEach(function(cat){
+        var imgTag = document.createElement('img');
+        imgTag.src = cat.photo;
+        imgTag.alt = "Photo of "+ cat.name;
+        catBoxes[i].append(imgTag);
+        i++;
+      });
+    });
+  });
 
 
 });
